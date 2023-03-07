991ac6f3ed690bece111fe9ab93586d208b5bf3e
Koposov Seryozha <koposov.serezha@gmail.com>
2023-03-07 13:33:37 +0300

    create Router

---
+++ NewsApp/Router.swift
@@ -0,0 +1,35 @@
+//
+//  Router.swift
+//  NewsApp
+//
+//  Created by user on 07.03.2023.
+//
+
+import UIKit
+
+protocol RouterMain {
+    var navigationController: UINavigationController? { get set }
+    var assemblyBuilder: AssemblyBuilderProtocol? { get set }
+
+}
+
+protocol RouterProtocol: RouterMain {
+    func initialViewController()
+}
+
+class Router: RouterProtocol {
+    var navigationController: UINavigationController?
+    var assemblyBuilder: AssemblyBuilderProtocol?
+
+    init(navigationController: UINavigationController? = nil, assemblyBuilder: AssemblyBuilderProtocol? = nil) {
+        self.navigationController = navigationController
+        self.assemblyBuilder = assemblyBuilder
+    }
+
+    func initialViewController() {
+        if let navigationController = navigationController {
+            guard let mainViewController = assemblyBuilder?.createMainModule(router: self) else { return }
+            navigationController.viewControllers = [mainViewController]
+        }
+    }
+}
